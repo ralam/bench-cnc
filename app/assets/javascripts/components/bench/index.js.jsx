e11b1a721902a95748bad0ca818805c7584211dc
ralam <rehan.alam@gmail.com>
2015-10-03 14:12:39 -0700

    add index react component

---
+++ app/assets/javascripts/components/bench/index.js.jsx
@@ -0,0 +1,31 @@
+(function () {
+  var Index = React.createClass({
+    getInitialState: function() {
+      return { benches: BenchStore.all() };
+    },
+
+    componentDidMount: function() {
+      BenchStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+      BenchStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange: function() {
+      this.setState({benches: BenchStore.all()});
+    },
+
+    render: function() {
+      return (
+        <div>
+          {
+            this.state.benches.map(function(bench){
+              return(<div>{ bench.description }</div>)
+            })
+          }
+        </div>
+      );
+    }
+  });
+});
