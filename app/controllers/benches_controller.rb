fc913551f5bfcd7007261f3a7b33e5d359463ebf
ralam <rehan.alam@gmail.com>
2015-10-03 12:48:13 -0700

    add bench controller

---
+++ app/controllers/benches_controller.rb
@@ -0,0 +1,21 @@
+class BenchesController < ApplicationController
+  def create
+    @bench = Bench.new(bench_params)
+    if @bench.save
+      render json: @bench
+    else
+      render json: @bench.errors.full_messages
+    end
+  end
+
+  def index
+    @benches = Bench.all
+    render json: @benches
+  end
+
+  private
+
+  def bench_params
+    params.require[:bench].permit[:description, :lat, :lng]
+  end
+end
