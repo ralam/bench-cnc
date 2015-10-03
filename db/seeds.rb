fbe47eff4f495b48a1d571397e7a61328cb8f749
ralam <rehan.alam@gmail.com>
2015-10-03 12:55:29 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Bench.create(description: 'SF Tech Shop bench', lat: 37.781220, lng: -122.405764)
+Bench.create(description: 'SOMA bench', lat: 37.777317, lng: -122.412237)
+Bench.create(description: 'Standard bench', lat: 37.780170, lng: -122.397298)
