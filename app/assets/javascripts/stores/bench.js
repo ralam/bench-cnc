5f1e1279b38c3467240cff04ab2f7c0306d04ea1
ralam <rehan.alam@gmail.com>
2015-10-03 13:50:12 -0700

    add change listeners

--- app/assets/javascripts/stores/bench.js
+++ app/assets/javascripts/stores/bench.js
@@ -1,15 +1,23 @@
 (function(root) {
+  var CHANGE_EVENT = "change";
   var _benches = [];
   var resetBenches = function(benches){
     _benches = benches;
-  }
+  },
   root.BenchStore = $.extend({}, EventEmitter.prototype, {
     all: function () {
       return _benches.slice(0);
     },
 
+    addChangeListener: function(callback) {
+      this.on(CHANGE_EVENT, callback);
+    },
+    removeChangeListener: fucntion(callback) {
+      this.removeListener(CHANGE_EVENT, callback);
+    },
     dispatcherID: AppDispatcher.register(function(payload){
       if(payload.actionType === BenchConstants.BENCHES_RECIEVED) {
+        BenchStore.emit(CHANGE_EVENT);
         resetBenches(payload.benches);
       }
     })
