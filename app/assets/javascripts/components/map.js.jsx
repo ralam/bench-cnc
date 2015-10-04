1ddbb4434021085c19921d12168d74f2209fec65
ralam <rehan.alam@gmail.com>
2015-10-03 21:15:37 -0700

    add map markers

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -1,5 +1,13 @@
 var Map = React.createClass({
-  componentDidMount: function(){
+  getInitialState: function() {
+    return {benches: BenchStore.all()}
+  },
+
+  componentDidMount: function() {
+    this.markers = []
+
+    BenchStore.addChangeListener(this._onChange);
+
     var map = React.findDOMNode(this.refs.map);
     var mapOptions = {
       center: {lat: 37.7758, lng: -122.435},
@@ -8,7 +16,24 @@ var Map = React.createClass({
     this.map = new google.maps.Map(map, mapOptions);
   },
 
+  _onChange: function() {
+    this.setState({benches: BenchStore.all()});
+  },
+
+  placeMarkers: function() {
+    this.state.benches.forEach(function(bench) {
+      var benchLatLng = new google.maps.LatLng(bench.lat, bench.lng)
+      var marker = new google.maps.Marker({
+        position: benchLatLng
+      });
+      this.markers.push(marker)
+      marker.setMap(this.map)
+    }.bind(this));
+  },
+
   render: function () {
+    this.placeMarkers();
+
     return (
       <div className="map" ref="map"></div>
     )
